e9b6f4af0723bef5e9603ee37cb256cf6f834e5f
Pavel Makhanko <neltarionzhs@gmail.com>
2025-06-14 13:33:52 +0200

    code adjustments

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import { config } from 'dotenv';
+import { rootCreateAction } from './commands/root-create';
 
-import axios from 'axios';
-import inquirer from 'inquirer';
 
 const program = new Command();
 
@@ -17,57 +15,6 @@ program
     .description('Create a root user')
     .option('-e, --email <email>', 'Root user email')
     .option('-p, --password <password>', 'Root user password')
-
-    .action(async (options) => {
-        try {
-            config();
-
-            const PORT = process.env.SERVER_PORT;
-
-            if (!PORT) {
-                throw new Error('SERVER_PORT is not set in .env file');
-            }
-
-            let rootEmail = options.email;
-            let rootPassword = options.password;
-
-            if (!rootEmail || !rootPassword) {
-                const answers = await inquirer.prompt([
-                    {
-                        type: 'input',
-                        name: 'rootEmail',
-                        message: '[avyyx] Enter root user email:',
-                        validate: (input) => {
-                            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-                            return emailRegex.test(input) ? true : 'Please enter a valid email address';
-                        },
-                        when: !rootEmail
-                    },
-                    {
-                        type: 'password',
-                        name: 'rootPassword',
-                        message: '[avyyx] Enter root user password:',
-                        validate: (input) => {
-                            return input.length >= 8 ? true : 'Password must be at least 8 characters long';
-                        },
-                        when: !rootPassword
-                    }
-                ]);
-
-                rootEmail = rootEmail || answers.rootEmail;
-                rootPassword = rootPassword || answers.rootPassword;
-            }
-
-
-            await axios.post(`http://localhost:${PORT}/init`, {
-                email: rootEmail,
-                password: rootPassword
-            });
-
-            console.log('[avyyx] Root user created');
-        } catch (error: any) {
-            throw new Error(error.message);
-        }
-    });
+    .action(rootCreateAction);
 
 program.parse(process.argv);
